2f56b73ba73b60882c7ee929ab648c9988828c47
RichardMathews <you@example.com>
2023-04-04 23:42:05 -0300

    Diretivas

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -6,6 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.css']
 })
 export class ListarPensamentoComponent implements OnInit {
+  listaPensamentos = [
+    {
+      conteudo: 'Poucos caracteres',
+      autoria: 'Dev',
+      modelo: 'modelo1'
+    },
+    {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componente pai',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Minha propiedade é decorada com @Input()',
+      autoria: 'Componente filho',
+      modelo: 'modelo2'
+    }
+  ]
 
   constructor() { }
 
